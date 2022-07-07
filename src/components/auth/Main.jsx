eaafd11064701cb7823a00aa53d0730a1828eba0
millerm30 <millergm30@gmail.com>
2022-07-07 17:19:33 -0230

    added username on login show

--- src/components/auth/Main.jsx
+++ src/components/auth/Main.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 const Main = () => {
+    const getCurrentUserEmail = () => {
+        const email = localStorage.getItem("emailData");
+        return email;
+    }
+
+    const getCurrentUser = () => {
+        const email = getCurrentUserEmail();
+        return {email};
+    }
+
     const handleLogOut = () => {
         localStorage.clear();
         window.location.reload();
@@ -9,6 +19,7 @@ const Main = () => {
     return (
         <div className='text-center mt-5'>
             <h1>This is a new screen! You have logged in!</h1>
+            <h2>{getCurrentUser().email} is currently logged in</h2>
             <button className="bg-blue-400 border-2 border-blue-500 py-2 px-4 rounded-lg mt-10 hover:bg-blue-500" onClick={handleLogOut}>Logout</button>
         </div>
     );
