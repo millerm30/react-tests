7006acc07efebc71760aff72efc541d7064af344
millerm30 <millergm30@gmail.com>
2022-07-07 16:48:51 -0230

    ....

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './components/auth/Login';
-import Layout from './components/layout/Layout';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const isLoggedIn = false;
 
-isLoggedIn ? root.render(<Layout isLoggedIn={isLoggedIn} />) : root.render(<Login />);
+root.render (
+    <Login />
+)
 
